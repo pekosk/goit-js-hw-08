e1ebcb3856cac89b5045064661c2b8daff9c30fa
pekosk <rkostyk64@gmail.com>
2021-11-16 16:16:50 +0200

    3 в процессе

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,11 @@
+import throttle from 'lodash.throttle';
+const form = document.querySelector('.feedback-form');
+const submitBtn = document.querySelector()
+const FORM_KEY = "feedback-form-state";
 
+const formInput = event => { localStorage.save(event.target.name, event.target.value);
+};
+
+const formSubmit = event => {
+  event.preventDefault();
+};
