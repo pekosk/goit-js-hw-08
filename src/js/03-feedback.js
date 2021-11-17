03c36187ac77125c99648f74c68964f296dbaf86
pekosk <rkostyk64@gmail.com>
2021-11-17 13:28:43 +0200

    3 таск

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,11 +1,39 @@
 import throttle from 'lodash.throttle';
-const form = document.querySelector('.feedback-form');
-const submitBtn = document.querySelector()
-const FORM_KEY = "feedback-form-state";
-
-const formInput = event => { localStorage.save(event.target.name, event.target.value);
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  textarea: document.querySelector('.feedback-form textarea'),
+  input: document.querySelector('.feedback-form input'),
 };
+const STORAGE_KEY = 'feedba0ck-form-state';
 
-const formSubmit = event => {
-  event.preventDefault();
-};
+let formData = {};
+refs.form.addEventListener('submit', onFormSubmit);
+refs.form.addEventListener('input', throttle(onTextareaInput, 500));
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+  console.log(`${refs.input.name}:`, refs.input.value);
+  console.log(`${refs.textarea.name}:`, refs.textarea.value);
+  evt.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+function onTextareaInput(evt) {
+  formData[refs.textarea.name] = refs.textarea.value;
+  formData[refs.input.name] = refs.input.value;
+  const saveDataEl = JSON.stringify(formData);
+  localStorage.setItem(STORAGE_KEY, saveDataEl);
+}
+
+const savedMessage = localStorage.getItem(STORAGE_KEY);
+const pasrsedSav = JSON.parse(savedMessage);
+
+function populateTextarea() {
+  if (savedMessage) {
+    const keys = Object.keys(pasrsedSav);
+    for (const key of keys) {
+      refs.form.elements[key].value = pasrsedSav[key];
+    }
+  }
+}
+populateTextarea();
